259c28796af5ca79ef709aeca360372aeb823ba4
haolei peng <hpen0004@student.monash.edu>
2021-10-14 01:04:20 +0800

    upload file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-  var spec2 = "js/visualization.vg.json";
+  var spec2 = "js/employment.vl.json";
+  //vegaEmbed('#employment_map', spec2);
   vegaEmbed('#employment_map', spec2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
